c19be43a916318392fb32ab604366bc883b97cbe
Benjamin Congdon <bencongdon96@gmail.com>
2020-05-16 17:24:02 -0700

    Async project changes

--- src/brave_clojure/core.clj
+++ src/brave_clojure/core.clj
@@ -1,5 +1,8 @@
 (ns brave-clojure.core
-  (:gen-class))
+  (:require [clojure.core.async
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread
+                     alts! alts!! timeout]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
